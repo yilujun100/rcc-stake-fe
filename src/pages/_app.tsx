1c2e0e938ab65423bf501dbabd71e15f6a993601
易陆军 <yilujun@jzsz.cc>
2024-10-16 17:42:15 +0800

    feat: config theme and toast

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,14 @@
 import '../styles/globals.css';
 import '@rainbow-me/rainbowkit/styles.css';
 import type { AppProps } from 'next/app';
+import { ThemeProvider } from '@mui/material/styles';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { WagmiProvider } from 'wagmi';
 import { RainbowKitProvider } from '@rainbow-me/rainbowkit';
 import { config } from '../wagmi';
+import theme from '../utils/theme';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import Head from 'next/head';
 import Layout from '../components/Layout';
 
@@ -21,13 +25,27 @@ function MyApp({ Component, pageProps }: AppProps) {
         />
         <link href="/favicon.ico" rel="icon" />
       </Head>
-      <WagmiProvider config={config}>
-        <QueryClientProvider client={client}>
-          <RainbowKitProvider>
-            <Layout><Component {...pageProps} /></Layout>
-          </RainbowKitProvider>
-        </QueryClientProvider>
-      </WagmiProvider>
+      <ThemeProvider theme={theme}>
+        <WagmiProvider config={config}>
+          <QueryClientProvider client={client}>
+            <RainbowKitProvider>
+              <ToastContainer
+                position="top-right"
+                autoClose={3000}
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+                theme="light"
+              />
+              <Layout><Component {...pageProps} /></Layout>
+            </RainbowKitProvider>
+          </QueryClientProvider>
+        </WagmiProvider>
+      </ThemeProvider>
     </>
   );
 }
